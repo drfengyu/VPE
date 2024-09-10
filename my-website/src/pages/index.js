31e8592dde1fd8591c2c9a9ccce733fa82a5fc5a
drfengyu <drfengling@163.com>
2024-09-10 11:08:10 +0800

    更新标题

--- my-website/src/pages/index.js
+++ my-website/src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            评估资料查阅 - 5min ⏱️
           </Link>
         </div>
       </div>
